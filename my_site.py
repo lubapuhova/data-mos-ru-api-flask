39e02905c2be6d91710ad3f66dff7ff68866c216
lubapuhova <luba_puhova@mail.ru>
2017-02-15 21:10:44 +0300

    H1

--- my_site.py
+++ my_site.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import requests
 
 app = Flask(__name__)
@@ -21,6 +21,17 @@ def show_table(n):
     row = data[n]
     return render_template("show_table.html",
                            table=row)
+
+@app.route('/json_table/<int:n>')
+def json_table (n):
+    data = get_data()
+    table = data[n]
+    x = []
+    y = []
+    for row in table['Cells']['IndexValues']:
+        x.append("{}, {}".format(row['Year'], row['Quarter']))
+        y.append(row['Value'])
+    return jsonify(x=x, y=y)
 
 if __name__ == '__main__':
     app.run()
